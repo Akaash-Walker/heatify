632b37e88cfd1ce26ab8d47cbcf8c7bdcebed308
Akaash-Walker <awalker7179@gmail.com>
2025-10-05 20:43:55 -0400

    add basic hero page

--- src/web/home.tsx
+++ src/web/home.tsx
@@ -1,15 +1,21 @@
 import Navbar from "./components/navbar.tsx";
-import LoginButton from "./components/loginButton.tsx";
 
 export default function Home() {
-
-    // display the message
     return (
-        <>
+        <div className="h-screen flex flex-col">
             <Navbar/>
-            <div className={"flex flex-col items-center justify-center"}>
-                <LoginButton/>
+            <div className="flex-1 relative">
+                <video
+                    src={"world_map.mp4"}
+                    autoPlay
+                    muted
+                    className={"w-full h-full object-cover absolute top-0 left-0"}
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-center w-1/3 ml-12 animate-fade-in">
+                    <span className="text-white text-8xl font-bold mb-8">Visualize Your Music</span>
+                    <button className="btn btn-primary">Try Now</button>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
