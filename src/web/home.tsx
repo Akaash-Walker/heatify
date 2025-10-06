5e577c8acd7e5534120d33d95342729b121590da
Akaash-Walker <awalker7179@gmail.com>
2025-10-05 20:05:03 -0400

    add auth0

--- src/web/home.tsx
+++ src/web/home.tsx
@@ -1,32 +1,14 @@
-import {useEffect, useState} from "react";
-// using axios instead of fetch for simplicity
-import axios from "axios";
 import Navbar from "./components/navbar.tsx";
+import LoginButton from "./components/loginButton.tsx";
 
 export default function Home() {
-    // useState to store the message contents
-    const [message, setMessage] = useState('');
-
-    // useEffect to fetch the message from the server
-    useEffect(() => {
-        axios.get('/api/')
-            .then(response => {
-                setMessage(response.data);
-            })
-            .catch(error => {
-                console.error('Error: ', error);
-            });
-        // runs once on mount
-    }, []);
 
     // display the message
     return (
         <>
             <Navbar/>
             <div className={"flex flex-col items-center justify-center"}>
-            <label className={"label text-center mt-10"}>
-                {message}
-            </label>
+                <LoginButton/>
             </div>
         </>
     );
--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from "./home.tsx";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home/>
-  </StrictMode>,
-)
+    <Auth0Provider
+        // move to env later
+        domain="dev-yhen7sk0jojn6bpe.us.auth0.com"
+        clientId="vKbeUBNPnB2LwlWTksn0npr7chbtqCmA"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <Home/>
+    </Auth0Provider>,
+);
