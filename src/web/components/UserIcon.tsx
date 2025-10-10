e0cfb915fb53dca52348faed688a41aff593ede1
crnorris1 <crnorris@wpi.edu>
2025-10-09 22:01:36 -0400

    Icon and welcome message

---
+++ src/web/components/UserIcon.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import {useState} from "react";
+
+export default function UserIcon(){
+    const access_token = localStorage.getItem('access_token');
+    const [icon, setIcon] = useState("");
+    
+    const getIcon = async function(){
+        if (!access_token) {
+            console.error("No access token found");
+            return;
+        }
+
+        try{
+            const res1 = await axios.get('https://api.spotify.com/v1/me', {
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded',
+                        'Authorization': 'Bearer ' + access_token
+                    },
+                })
+            const userIcon = res1.data.images[0].url;
+            setIcon(userIcon);
+        } catch (error) {
+            console.error("Error fetching user email: ", error);
+        }
+    }
+    
+    getIcon();
+
+    return(
+        <img src= {icon} className= "w-32 h-32 rounded-full object-cover" style={{
+                position: "absolute", 
+                top: "10px",          
+                right: "10px",  
+                width: 50, height: 50,      
+        }}/>
+    )
+
+}
--- src/web/components/noUserNav.tsx
+++ src/web/components/noUserNav.tsx
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router";
+import UserIcon from "./UserIcon.tsx"
+import WelcomeMessage from "./WelcomeMessage.tsx"
 
 export default function NoUserNav() {
     const navigate = useNavigate();
@@ -6,7 +8,18 @@ export default function NoUserNav() {
     return (
         <div className="navbar bg-base-100 shadow-sm px-8">
             <div className="navbar-start">
-                <a className="btn btn-ghost text-xl" onClick={() => navigate("/")}>Heatify</a>
+                <img className="btn btn-ghost text-xl" 
+                    src= {'public/HeatifyLogo.png'} 
+                    width={50} height={50} 
+                    alt='Heatify Logo' 
+                    style={{
+                        padding: "0px", 
+                    }}
+                    onClick={() => navigate("/")}/>
+
+                <b style ={{top: "10px", left: "50px"}}>Heatify</b>
+                <WelcomeMessage />
+                <UserIcon />
             </div>
         </div>
     );
