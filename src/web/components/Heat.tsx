7a9c6df6fddcbd063619e287cbf522504c90cb6e
Akaash-Walker <awalker7179@gmail.com>
2025-10-09 21:29:58 -0400

    add suspense and speed up load of Heat.tsx

--- src/web/components/Heat.tsx
+++ src/web/components/Heat.tsx
@@ -1,49 +1,46 @@
-import {useState, useEffect} from 'react'
-import Heatmap from "./Heatmap"
+import {useState, useEffect, Suspense, lazy} from 'react'
 import Legend from "./Legend"
 import LoadCountriesTask from '../../tasks/LoadCountriesTask'
 import legendItems from '../../tasks/LegendItems';
 import NoUserNav from "./noUserNav.tsx";
+import Loading from "../loading.tsx";
 
+const Heatmap = lazy(() => import('./Heatmap'));
 
+type CountryGeoJSON = GeoJSON.FeatureCollection;
 
 const Heat = () => {
-    const [countries, setCountries] = useState<any[]>([]);
+    const [countries, setCountries] = useState<CountryGeoJSON[] | null>(null);
     const legendItemsInReverse = [...legendItems].reverse()
 
-    const load = () => {
-        const loadCountriesTask = new LoadCountriesTask();
-        loadCountriesTask.load(setCountries);
-    };
-
-    // grabs access token from URL hash and stores it in localStorage
-    // needed for making authenticated requests to Spotify API
-    const storeAccessToken = () => {
+    // on component mount, store access token and load country data
+    useEffect(() => {
         const hash = window.location.hash.substring(1);
         const params = new URLSearchParams(hash);
         const accessToken = params.get('access_token');
         if (accessToken) {
             localStorage.setItem('access_token', accessToken);
-            // Optionally, remove the token from the URL
             window.location.hash = '';
         }
-    }
+        const loadCountriesTask = new LoadCountriesTask();
+        loadCountriesTask.load(setCountries);
+    }, []);
 
-    // on component mount, store access token and load country data
-    useEffect(() => {
-        storeAccessToken();
-        load();
-    }, [countries]);
-
-    return(
+    return (
         <div>
-            <NoUserNav/>
-            {countries.length === 0 ? <div>No Data</div> : <div>
-                <Heatmap countries={countries}/>
-                <Legend legendItems={legendItemsInReverse}/>
-                </div>}
+            <Suspense fallback={<Loading />}>
+                {!countries ? (
+                    <Loading />
+                ) : (
+                    <>
+                        <NoUserNav />
+                        <Heatmap countries={countries} />
+                        <Legend legendItems={legendItemsInReverse} />
+                    </>
+                )}
+            </Suspense>
         </div>
-    )
+    );
 }
 
 export default Heat
